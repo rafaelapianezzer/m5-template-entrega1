e2524a05d9aa486c41e6edb288758fef96202561
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-11-20 16:02:28 -0300

    test setup + creating task tests

---
+++ src/tests/createTask.test.ts
@@ -0,0 +1,20 @@
+import { describe, it } from "vitest";
+import { request } from "./setupFiles";
+import { taskDefaultExpects } from "./utils/taskDefaultExpects";
+import { task, taskWithCategory } from "./mocks/tasks.mocks";
+
+describe("create task", () => {
+   it("should be able to create task correctly", async () => {
+      const data = await request
+         .post("/task")
+         .send(task)
+         .expect(201)
+         .then((response) => response.body);
+
+      taskDefaultExpects(data);
+   });
+
+   it("should throw error when try to create a task in a invalid category", async () => {
+      await request.post("/task").send(taskWithCategory).expect(403);
+   });
+});
---
+++ src/tests/mocks/tasks.mocks.ts
@@ -0,0 +1,10 @@
+export const task = {
+    title: "Lorem ipsum",
+    content: "Lorem ipsum",
+}
+
+export const taskWithCategory = {
+    title: "Lorem ipsum",
+    content: "Lorem ipsum",
+    categoryId: 1,
+}
