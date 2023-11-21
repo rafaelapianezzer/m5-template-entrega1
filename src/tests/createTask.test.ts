97d46e05f6b026cba4ddf376dcccf0cc6afe1d6a
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-11-21 11:05:24 -0300

    validation and serialization tests for create task

--- src/tests/createTask.test.ts
+++ src/tests/createTask.test.ts
@@ -1,10 +1,10 @@
 import { describe, it } from "vitest";
 import { request } from "./setupFiles";
 import { taskDefaultExpects } from "./utils/taskDefaultExpects";
-import { task, taskWithCategory } from "./mocks/tasks.mocks";
+import { invalidDataTask, task, taskWithInvalidCategory } from "./mocks/tasks.mocks";
 
 describe("create task", () => {
-   it("should be able to create task correctly", async () => {
+   it("should be able to create task sucessfully", async () => {
       const data = await request
          .post("/tasks")
          .send(task)
@@ -15,6 +15,14 @@ describe("create task", () => {
    });
 
    it("should throw error when try to create a task in a invalid category", async () => {
-      await request.post("/tasks").send(taskWithCategory).expect(403);
+      await request.post("/tasks").send(taskWithInvalidCategory).expect(403);
    });
+
+   it("should throw error when try to create a task with a missing body parameter", async () => {
+      await request.post("/tasks").expect(409);
+   })
+
+   it("should throw error when try to create a task with invalid data types", async () => {
+      await request.post("/tasks").send(invalidDataTask).expect(409);
+   })
 });
