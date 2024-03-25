a29c8e4defb6aca9af6af6026f30403d69638c8b
Cauan <cauan.f.ramos@gmail.com>
2024-03-25 16:03:53 -0300

    added tsconfig file and validated tests

--- src/tests/specs/tasks/deleteTasks.test.ts
+++ src/tests/specs/tasks/deleteTasks.test.ts
@@ -4,22 +4,16 @@ import { task } from "../../mocks/tasks.mocks";
 import { request } from "../../setupFiles";
 
 describe("delete task", () => {
-    beforeEach(async () => {
-        await prisma.task.create({ data: task });
-    })
+  beforeEach(async () => {
+    await prisma.task.create({ data: task });
+  });
 
-    it("should be able to delete task sucessfully", async () => {
-        const task = await prisma.task.findFirst();
+  it("should be able to delete task successfully", async () => {
+    const task = await prisma.task.findFirst();
+    await request.delete(`/tasks/${task?.id}`).expect(204);
+  });
 
-        await request.delete(`/tasks/${task?.id}`).expect(204);
-    })
-
-
-    it("should throw error when try to delete a invalid task", async () => {
-        const task = await prisma.task.findFirst();
-
-        const id = (task?.id as number) + 1;
-
-        await request.delete(`/tasks/${id}`).expect(404);
-    })
-})
+  it("should throw error when try to delete a invalid task", async () => {
+    await request.delete(`/tasks/999`).expect(404);
+  });
+});
--- src/tests/specs/tasks/updateTask.test.ts
+++ src/tests/specs/tasks/updateTask.test.ts
@@ -1,47 +1,50 @@
 import { beforeEach, describe, expect, it } from "vitest";
-import { request } from "../../setupFiles";
-import { getTaskList, invalidDataUpdateTask, updateTask } from "../../mocks/tasks.mocks";
 import { prisma } from "../../../database/prisma";
 import { category } from "../../mocks/category.mocks";
+import {
+   getTaskList,
+   invalidDataUpdateTask,
+   updateTask,
+} from "../../mocks/tasks.mocks";
+import { request } from "../../setupFiles";
 import { taskDefaultExpects } from "../../utils/taskDefaultExpects";
 
 describe("update task", () => {
-   beforeEach(async () => {
-      await prisma.category.create({ data: category });
-      const taskList = await getTaskList();
-      await prisma.task.createMany({ data: taskList });
-   });
+  beforeEach(async () => {
+    await prisma.category.create({ data: category });
+    const taskList = await getTaskList();
+    await prisma.task.createMany({ data: taskList });
+  });
 
-   it("should be able to update task successfully ", async () => {
-      const task = await prisma.task.findFirst();
+  it("should be able to update task successfully ", async () => {
+    const task = await prisma.task.findFirst();
 
-      const data = await request
-         .patch(`/tasks/${task?.id}`)
-         .send(updateTask)
-         .expect(200)
-         .then((response) => response.body);
+    const data = await request
+      .patch(`/tasks/${task?.id}`)
+      .send(updateTask)
+      .expect(200)
+      .then((response) => response.body);
 
-      taskDefaultExpects(data);
+    taskDefaultExpects(data);
 
-      expect(data.title).toBe(updateTask.title);
-      expect(data.content).toBe(updateTask.content);
-      expect(data.finished).toBe(updateTask.finished);
-   });
+    expect(data.title).toBe(updateTask.title);
+    expect(data.content).toBe(updateTask.content);
+    expect(data.finished).toBe(updateTask.finished);
+  });
 
-   it("should throw error when try to update a invalid task", async () => {
-      const tasks = await prisma.task.findMany();
+  it("should throw error when try to update a invalid task", async () => {
+    await request
+      .patch("/tasks/999")
+      .expect(404)
+      .then((response) => response.body);
+  });
 
-      const id = tasks[1].id + 1;
+  it("should throw error when try to update a task with invalid data types", async () => {
+    const task = await prisma.task.findFirst();
 
-      await request
-         .patch(`/tasks/${id}`)
-         .expect(404)
-         .then((response) => response.body);
-   })
-
-   it("should throw error when try to update a task with invalid data types", async () => {
-      const task = await prisma.task.findFirst();
-
-      await request.patch(`/tasks/${task?.id}`).send(invalidDataUpdateTask).expect(400);
-   })
+    await request
+      .patch(`/tasks/${task?.id}`)
+      .send(invalidDataUpdateTask)
+      .expect(400);
+  });
 });
