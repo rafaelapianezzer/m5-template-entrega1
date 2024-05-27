b914319b39d6a77aa808923301acb538534c9387
Chrystian Rodolfo <87505223+kenzie-chrystian@users.noreply.github.com>
2024-05-27 13:16:17 -0300

    Update deleteTasks.test.ts

--- src/tests/specs/tasks/deleteTasks.test.ts
+++ src/tests/specs/tasks/deleteTasks.test.ts
@@ -1,19 +1,29 @@
-import { beforeEach, describe, it } from "vitest";
+import { beforeEach, describe, it, expect } from "vitest";
 import { prisma } from "../../../database/prisma";
-import { task } from "../../mocks/tasks.mocks";
 import { request } from "../../setupFiles";
 
 describe("delete task", () => {
   beforeEach(async () => {
+    const task = {
+      title: "Lorem ipsum",
+      content: "Lorem ipsum",
+    };
     await prisma.task.create({ data: task });
   });
 
-  it("should be able to delete task successfully", async () => {
+  it("should be able to delete a task successfully", async () => {
     const task = await prisma.task.findFirst();
     await request.delete(`/tasks/${task?.id}`).expect(204);
   });
 
-  it("should throw error when try to delete a invalid task", async () => {
-    await request.delete(`/tasks/999`).expect(404);
+  it("should return an error when deleting a task with non existing id", async () => {
+    const response = await request.delete(`/tasks/99999`);
+
+    const expectedBody = {
+      message: "Task not found",
+    };
+
+    expect(response.body).toEqual(expectedBody);
+    expect(response.statusCode).toBe(404);
   });
 });
